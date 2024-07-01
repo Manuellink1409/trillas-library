84aa6edeea7fb3056c91b57f9d16de06579596f1
Manuellink1409 <manuelaft14@gmail.com>
2024-07-01 13:08:46 -0600

    Initial Docker Setup

---
+++ trillas-library/src/main/java/com/hypermedia/library/TrillasLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.hypermedia.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrillasLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrillasLibraryApiApplication.class, args);
+	}
+
+}
---
+++ trillas-library/src/test/java/com/hypermedia/library/TrillasLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypermedia.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrillasLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
