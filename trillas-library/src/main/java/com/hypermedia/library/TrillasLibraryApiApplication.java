ede010a0cfcc2dcf374b9c6e886ba12bab2a02eb
Manuellink1409 <manuelaft14@gmail.com>
2024-07-01 13:33:10 -0600

    Adding User model

--- trillas-library/src/main/java/com/hypermedia/library/TrillasLibraryApiApplication.java
+++ trillas-library/src/main/java/com/hypermedia/library/TrillasLibraryApiApplication.java
@@ -2,8 +2,10 @@ package com.hypermedia.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TrillasLibraryApiApplication {
 
 	public static void main(String[] args) {
